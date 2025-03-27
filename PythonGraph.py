4c4d1c08cca29c4486eb659c4270bc4cd91c84ff
Khalil <khalilftw0@gmail.com>
2025-03-27 13:32:33 +0000

    Graph API is now working through Python, just have to add some real functionality

--- PythonGraph.py
+++ PythonGraph.py
@@ -1,10 +1,41 @@
 # Python Graph - Script by Khalil
-# MS Docs: https://github.com/microsoftgraph/msgraph-sdk-python // https://learn.microsoft.com/en-us/graph/tutorials/python?tabs=aad
+# MS Docs: https://github.com/microsoftgraph/msgraph-sdk-python
 # .\env\Scripts\Activate.ps1
 from dotenv import load_dotenv
+from azure.identity import InteractiveBrowserCredential
+from msgraph import GraphServiceClient
 import os
+import asyncio
 
 load_dotenv()
 
-print(f"Client ID: {os.getenv('CLIENT_ID')}")
-print(f"Tenant ID: {os.getenv('TENANT_ID')}")
+credential = InteractiveBrowserCredential(
+    client_id=os.getenv('CLIENT_ID'),
+    tenant_id=os.getenv('TENANT_ID'),
+    redirect_uri='http://localhost:8400' # Hosts on this port until Auth is finished
+)
+scopes = ["User.Read"]
+
+# credential = DefaultAzureCredential()
+# scopes = ["https://graph.microsoft.com/.default"]
+
+client = GraphServiceClient(credentials=credential, scopes=scopes)
+
+async def GetME():
+    try:
+        info = await client.me.get()
+    except KeyboardInterrupt:
+        print("Exiting due to CTRL-C") 
+    except Exception as ex:
+        print(f"Error: {ex}")
+        exit()
+
+    if info:
+        print(info.display_name)
+        print(info.user_principal_name)
+        print(info.id)
+
+asyncio.run(GetME())
+
+# print(f"Client ID: {os.getenv('CLIENT_ID')}")
+# print(f"Tenant ID: {os.getenv('TENANT_ID')}")
